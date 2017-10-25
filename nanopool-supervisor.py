e1540614b5416c94c60ed5940f2f86e80b8da2e1
Jusber <jussi.ratilainen@coheros.com>
2017-10-25 11:18:44 +0300

    Log to file

--- nanopool-supervisor.py
+++ nanopool-supervisor.py
@@ -1,50 +1,48 @@
 
 #!/usr/bin/python
 
-import requests, sys, argparse, traceback, time, threading, os
+import requests, sys, argparse, traceback, time, threading, os, logging
 from argparse import RawTextHelpFormatter
 
 NANOPOOL_HASHRATE_TEMPLATE = 'https://api.nanopool.org/v1/eth/hashrate/$ADDRESS$'
 
-def debug(text):
-  if(DEBUG):
-    print(text)
+scriptLocation = os.path.dirname(os.path.realpath(__file__))
 
 def checkHashRate():
-    debug('checkHashRate()')
+    logging.debug('checkHashRate()')
     err = 0
     try:
         r = requests.get(NANOPOOL_HASHRATE_TEMPLATE.replace('$ADDRESS$', nanoPoolHashRateAddress))
         if(r.status_code >= 400):
-            print('Getting HashRate failed, nanopool maybe broken or wrong address?. status_code:' + str(r.status_code))
+            logging.warning('Getting HashRate failed, nanopool maybe broken or wrong address?. status_code:' + str(r.status_code))
             return 3
-        debug('Response: ' + r.text)
+        logging.debug('Response: ' + r.text)
         status = r.json()['status']
         hashRate = r.json()['data']
         if(status == True):
             if(hashRate >= hashRateLowerLimit):
-                debug('Hash rate ok(' + str(hashRate) + ')')
+                logging.debug('Hash rate ok(' + str(hashRate) + ')')
                 return 0
             else:
-                print('Hash rate is smaller(' + str(hashRate)  + ') than wanted(' + str(hashRateLowerLimit) + '). This might cause recovery boot soonish.')
+                logging.warning('Hash rate is smaller(' + str(hashRate)  + ') than wanted(' + str(hashRateLowerLimit) + '). This might cause recovery boot soonish.')
                 return 1
         else:
-            print('TODO: Status is not true. No freaking idea what that means? I guess we report error.')
+            logging.warning('TODO: Status is not true. No freaking idea what that means? I guess we report error.')
             return 1
     except Exception as e:
-        print('Getting HashRate failed, nanopool, internet connection or this code maybe broken?')
-        print("Unexpected error:" + str(traceback.format_exc()))
+        logging.warning('Getting HashRate failed, nanopool, internet connection or this code maybe broken?')
+        logging.warning("Unexpected error:" + str(traceback.format_exc()))
         return 2
     return err
 
 def rebootThread(waitTime, stop_event):
-    debug('Starting reboot thread, booting in ' + str(waitTime) + ' seconds if no change in conditions...')
+    logging.debug('Starting reboot thread, booting in ' + str(waitTime) + ' seconds if no change in conditions...')
     stop_event.wait(waitTime)
     if(stop_event.isSet()):
-        print('Critical thread Reboot cancelled')
+        logging.warning('Critical thread Reboot cancelled')
         return
-    print('Booting...')
-    #os.system('reboot --reboot')
+    logging.warning('Booting...')
+    os.system('reboot --reboot')
 
 
 parser = argparse.ArgumentParser(description='This script is used to check if \
@@ -58,22 +56,34 @@ parser.add_argument("--interval", help='How often(minutes) to check the hash rat
 parser.add_argument("--serious_boot_interval", help='How fast to reboot after serious problems are detected', default=20, type=int)
 parser.add_argument("--unsure_boot_interval", help='How fast to reboot after unsure problems are detected', default=60, type=int)
 parser.add_argument("--hash_rate_limit", help='Hash rate supervision limit(Mh/s', default=1, type=int)
-parser.add_argument("-v", "--verbose", action="store_true",
-                    help="Print debug logs to stdout")
+parser.add_argument("-v", "--verbose", action="store_true", help="Print debug logs")
+parser.add_argument("-l", "--log_to_file", action="store_true", help="Print to file, instead of stdout")
 parser.add_argument("address", help='ethereum address')
 
 args = parser.parse_args()
-DEBUG = args.verbose
+debug = args.verbose
 nanoPoolHashRateAddress = args.address
 interval = args.interval
 seriousBootInterval = args.serious_boot_interval
 unsureBootInterval = args.unsure_boot_interval
 hashRateLowerLimit = args.hash_rate_limit
+logToFile = args.log_to_file
 
-print('Supervising account: ' + nanoPoolHashRateAddress)
-print('Checking hash rate in every ' + str(interval) + ' minutes')
-print('Booting computer if hash rate is below ' + str(hashRateLowerLimit))
-print('Booting computer after ' + str(seriousBootInterval) + ' minutes if serious problems are detected and after ' + str(unsureBootInterval) + ' minutes if unsure problems are detected.')
+loggingLevel=None
+if(debug):
+    loggingLevel = logging.DEBUG
+else:
+    loggingLevel = logging.INFO
+
+if(logToFile):
+    logging.basicConfig(filename=scriptLocation + '/' + os.path.splitext(__file__)[0] + '.log', level=loggingLevel)
+else:
+    logging.basicConfig(level=loggingLevel)
+
+logging.info('Supervising account: ' + nanoPoolHashRateAddress)
+logging.info('Checking hash rate in every ' + str(interval) + ' minutes')
+logging.info('Booting computer if hash rate is below ' + str(hashRateLowerLimit))
+logging.info('Booting computer after ' + str(seriousBootInterval) + ' minutes if serious problems are detected and after ' + str(unsureBootInterval) + ' minutes if unsure problems are detected.')
 
 serious_thread_stop = None
 serious_thread = None
@@ -83,29 +93,29 @@ unsure_thread = None
 while(1):
     ret = checkHashRate()
     if(ret == 0):
-        debug('All OK')
+        logging.debug('All OK')
         if(serious_thread and serious_thread.isAlive()):
             serious_thread_stop.set()
         if(unsure_thread and unsure_thread.isAlive()):
             unsure_thread_stop.set()
     elif(ret == 1):
-        print('Serious problems detected in Hashrate')
+        logging.warning('Serious problems detected in Hashrate')
         if(serious_thread and serious_thread.isAlive()):
-            debug('Serious thread seems to be running already')
+            logging.debug('Serious thread seems to be running already')
         else:
             serious_thread_stop = threading.Event()
             serious_thread = threading.Thread(target=rebootThread, args=(seriousBootInterval*60, serious_thread_stop))
             serious_thread.start()
     elif(ret == 2):
-        print('Maybe fatal problems detected in Hashrate')
+        logging.warning('Maybe fatal problems detected in Hashrate')
         if(unsure_thread and unsure_thread.isAlive()):
-            debug('Unsure thread seems to be running already')
+            logging.debug('Unsure thread seems to be running already')
         else:
             unsure_thread_stop = threading.Event()
             unsure_thread = threading.Thread(target=rebootThread, args=(unsureBootInterval*60, unsure_thread_stop))
             unsure_thread.start()
     elif(ret == 3):
-        print('Likely not fatal problems detected in Hashrate, not doing anything')
+        logging.warning('Likely not fatal problems detected in Hashrate, not doing anything')
     else:
-        print('Unknown code')
+        logging.info('Unknown code')
     time.sleep(interval*60)
