208b689014a837416066241827b3baa395401364
Jusber <jussi.ratilainen@coheros.com>
2017-11-13 11:07:39 +0200

    set -eu for bash, just in case

--- get.sh
+++ get.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+set -eu
 wget https://raw.githubusercontent.com/JusbeR/ethereumpool-supervisor/master/ethereumpool-supervisor.py
 wget https://raw.githubusercontent.com/JusbeR/ethereumpool-supervisor/master/ethermine.py
 wget https://raw.githubusercontent.com/JusbeR/ethereumpool-supervisor/master/nanopool.py
