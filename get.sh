07707bfb9b79c4e62a8d148582619e1a86079672
Jusber <jussi.ratilainen@coheros.com>
2017-11-13 11:04:19 +0200

    +x

--- get.sh
+++ get.sh
@@ -3,3 +3,4 @@
 wget https://raw.githubusercontent.com/JusbeR/ethereumpool-supervisor/master/ethereumpool-supervisor.py
 wget https://raw.githubusercontent.com/JusbeR/ethereumpool-supervisor/master/ethermine.py
 wget https://raw.githubusercontent.com/JusbeR/ethereumpool-supervisor/master/nanopool.py
+chmod +x ethereumpool-supervisor.py
